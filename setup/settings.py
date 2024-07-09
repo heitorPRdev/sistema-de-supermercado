09ea255954b1215ccf521b22c1bfec5e2e5442f1
heitorPrDev <herohecttorplay@gmail.com>
2024-07-09 02:38:28 -0300

    1.0.0-Restauração-

--- setup/settings.py
+++ setup/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'supermercado.apps.SupermercadoConfig'
+    'supermercado.apps.SupermercadoConfig',
 ]
 
 MIDDLEWARE = [
--- supermercado/views.py
+++ supermercado/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login as auth_login
 
+from django.http.response import HttpResponse
 # Create your views here.
 def cadastro(request):
-    return render(request,'cadastro.html')
+    if request.method == 'GET':
+        return render(request,'cadastro.html')
+    else:
+        nome = request.POST.get('username')
+        senha = request.POST.get('password')
+        users = User.objects.filter(username=nome).first()
+        if users:
+            return render(request,'cadastro.html', {'erro':'Desculpe já exite um usuario com esse nome'})
+        users = User.objects.create_user(username=nome,password=senha)
+        users.save()
+        return redirect('login')
+
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'GET':
+        return render(request,'login.html')
+    else:
+        nome = request.POST.get('username')
+        senha = request.POST.get('password')
+        users = authenticate(username=nome,password=senha)
+        if users:
+            auth_login(request,users)
+            return render(request,'login.html', {'erro':f'Sucesso'})
+        
+        return render(request,'login.html', {'erro':f'Desculpe usuario não encontrado'})
+
+
+def AddProdutos(request):
+    if request.is_authenticated():
+        return render(request, 'addprodutos.html')
+    return redirect('cadastro')
